0118020bb7091341fae1ad49d5dd9d70ef643cf2
Seyar Chapuh <seyar@yandex-team.ru>
2015-07-08 00:19:20 +0300

    some refactoring. Only one method parse

--- config.js
+++ config.js
@@ -5,12 +5,23 @@ module.exports = {
                 rss: [],
                 html: {
                     'http://eda.ru': {
+                        amountCategories: 1,
+                        amountPostsForEachCategory: 1,
                         categories: {
                             url: '/recipes',
                             querySelector: '.b-tag-menu-items a[id^="link-recipecatalog"]'
                         },
                         postsLinks: {
                             querySelector: '.b-recipe-widget__name a[id^="link-recipewidget"]'
+                        },
+                        post: {
+                            title: '.b-directions h2',
+                            text: '.b-directions .instructions',
+                            ingridients: [
+                                '.ingredient a[id^="link-recipe-ingredient"]',
+                                '.ingredient .ingredient-measure-amount span'
+                            ],
+                            video: 'object'
                         }
                     }
                 }
